f63323e01a70a8587bff015ba0633ccfb2ee3c3c
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-19 12:48:39 +0530

    WPF-56519 - Sample and screenshot added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+using System.Windows;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("AR");
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.FlowDirection = FlowDirection.RightToLeft;
+        }
+    }
+}
+
